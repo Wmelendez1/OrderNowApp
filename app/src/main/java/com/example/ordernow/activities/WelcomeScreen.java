447f0703cac0a54943a4f270fafc6f0ea40b0541
DPerez007 <danielperez078@gmail.com>
2024-05-09 19:41:11 -0400

    Add Sign Up Logic Add Firebase Authentication

--- app/src/main/java/com/example/ordernow/activities/WelcomeScreen.java
+++ app/src/main/java/com/example/ordernow/activities/WelcomeScreen.java
@@ -1,22 +1,39 @@
 package com.example.ordernow.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 import com.example.ordernow.R;
 
+import com.example.ordernow.databinding.ActivitySignUpBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class WelcomeScreen extends AppCompatActivity {
 
+    FirebaseAuth mAuth;
+    FirebaseDatabase database;
+    ActivitySignUpBinding binding;
+    public String TAG = "StringTag";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+
         super.onCreate(savedInstanceState);
+        database = FirebaseDatabase.getInstance();
+        mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_main);
 
         //set buttons here
         Button signUpButton = findViewById(R.id.signupButton);
 
+        setVariable();
+        getWindow().setStatusBarColor(getResources().getColor(R.color.themecolor));
         //on click listeners here
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,5 +44,17 @@ public class WelcomeScreen extends AppCompatActivity {
             }
         });
     }
+
+    private void setVariable() {
+        binding.loginreg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        binding.signupButton.setOnClickListener(v -> startActivity(new Intent(WelcomeScreen.this, SignUp.class)));
+    }
+
 }
 
